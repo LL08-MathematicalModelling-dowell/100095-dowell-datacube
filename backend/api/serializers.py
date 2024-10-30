2da6027eb09b53964fc786282590ec6355f4e6cf
Goldeno10 <ibrahimmuhammad271@gmail.com>
2024-10-30 17:02:05 +0100

    Remove API data_type requirement

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -9,7 +9,7 @@ class InputGetSerializer(serializers.Serializer):
         ('delete', 'delete'),
         ('fetch', 'fetch'),
     ]
-    choose_data_type = ['real_data', 'testing_data', 'learning_data', 'deleted_data']
+    # choose_data_type = ['real_data', 'testing_data', 'learning_data', 'deleted_data']
 
     coll_name = serializers.CharField(max_length=255, required=True)
     db_name = serializers.CharField(max_length=255, required=True)
@@ -19,7 +19,7 @@ class InputGetSerializer(serializers.Serializer):
     limit = serializers.IntegerField(required=False)
     offset = serializers.IntegerField(required=False)
     # payment = serializers.BooleanField(default=True, allow_null=True, required=False)
-    data_type = serializers.ChoiceField(choices=choose_data_type, required=True)
+    # data_type = serializers.ChoiceField(choices=choose_data_type, required=True)
 
 
 class InputPostSerializer(serializers.Serializer):
@@ -29,7 +29,7 @@ class InputPostSerializer(serializers.Serializer):
         ('delete', 'delete'),
         ('fetch', 'fetch'),
     ]
-    choose_data_type = ['real_data', 'testing_data', 'learning_data', 'deleted_data']
+    # choose_data_type = ['real_data', 'testing_data', 'learning_data', 'deleted_data']
     
     # api_key = serializers.CharField(max_length=510, required=True)
     coll_name = serializers.CharField(max_length=255, required=True)
@@ -38,11 +38,11 @@ class InputPostSerializer(serializers.Serializer):
     data = serializers.JSONField(required=True)
     # payment = serializers.BooleanField(default=True, allow_null=True, required=False)
     # is_deleted = serializers.BooleanField(default=False)
-    data_type = serializers.ChoiceField(choices=choose_data_type, required=True)
+    # data_type = serializers.ChoiceField(choices=choose_data_type, required=True)
 
 
 class InputPutSerializer(serializers.Serializer):
-    choose_data_type = ['real_data', 'testing_data', 'learning_data', 'deleted_data']
+    # choose_data_type = ['real_data', 'testing_data', 'learning_data', 'deleted_data']
     # api_key = serializers.CharField(max_length=510, required=True)
     db_name = serializers.CharField(max_length=100)
     coll_name = serializers.CharField(max_length=100)
@@ -50,17 +50,17 @@ class InputPutSerializer(serializers.Serializer):
     query = serializers.JSONField(required=False)
     update_data = serializers.JSONField(required=False)
     # payment = serializers.BooleanField(default=True, allow_null=True, required=False)
-    data_type = serializers.ChoiceField(choices=choose_data_type, required=True)
+    # data_type = serializers.ChoiceField(choices=choose_data_type, required=True)
 
 
 class InputDeleteSerializer(serializers.Serializer):
-    choose_data_type = ['real_data', 'testing_data', 'learning_data', 'deleted_data']
+    # choose_data_type = ['real_data', 'testing_data', 'learning_data', 'deleted_data']
     # api_key = serializers.CharField(max_length=510)
     db_name = serializers.CharField(max_length=100)
     coll_name = serializers.CharField(max_length=100)
     operation = serializers.CharField(max_length=10)
     query = serializers.JSONField(required=False)
-    data_type = serializers.ChoiceField(choices=choose_data_type, required=True)
+    # data_type = serializers.ChoiceField(choices=choose_data_type, required=True)
 
 
 class NotEmptyStringValidator:
