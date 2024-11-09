a3d5d1f799f77f00a74229c8ff76f6fe87e8a1a9
Goldeno10 <ibrahimmuhammad271@gmail.com>
2024-11-09 17:22:51 +0100

    Updated, Optimized API logics

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import (
     DataCrudView,
-    CreateDatabaseView, ListCollectionsView,
-    AddCollectionView, api_home,
+    CreateDatabaseView,
+    ListCollectionsView,
+    AddCollectionView,
+    ListDatabasesView,
+    DropDatabaseView,
+    api_home,
 )
 
 app_name = 'api'
@@ -13,4 +17,6 @@ urlpatterns = [
     path('api/create_database/', CreateDatabaseView.as_view(), name='create_database'),
     path('api/list_collections/', ListCollectionsView.as_view(), name='list_collections'),
     path('api/add_collection/', AddCollectionView.as_view(), name='add_collection'),
+    path('api/list_databases/', ListDatabasesView.as_view(), name='list_databases'),
+    # path('api/drop_database/', DropDatabaseView.as_view(), name='drop_database'),
 ]
