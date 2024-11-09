e0c3c8356a9f797dd2d2e8d72116d53d38136121
Goldeno10 <ibrahimmuhammad271@gmail.com>
2024-11-09 17:23:48 +0100

    Implemeted Logging functionality

--- backend/api/views.py
+++ backend/api/views.py
@@ -14,7 +14,6 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
 
 # from api.helpers import check_api_key
 from api.serializers import (
