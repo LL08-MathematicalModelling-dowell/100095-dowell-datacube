3519908ef0a8a88aa5ca1561d9a4e3ded6bf7fbd
Goldeno10 <ibrahimmuhammad271@gmail.com>
2025-08-14 10:13:41 +0100

    ..1

--- backend/core/utils/db.py
+++ backend/core/utils/db.py
@@ -1,16 +1,15 @@
 import os
+from django.conf import settings
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
-MONGO_DB_NAME = os.getenv("AUTH_DB_NAME")
 
 class MongoConnection:
     def __init__(self):
-        self.client = MongoClient(MONGO_URI)
-        self.db = self.client[MONGO_DB_NAME]
+        self.client = MongoClient(settings.MONGODB_URI)
+        self.db = self.client[settings.DATACUBE_V2_AUTH_DB]
 
     def get_collection(self, collection_name):
         return self.db[collection_name]
