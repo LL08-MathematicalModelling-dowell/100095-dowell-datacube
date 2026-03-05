50f39fed039065503cc7803c4dcca3c560d92fe3
Goldeno10 <ibrahimmuhammad271@gmail.com>
2026-03-05 20:27:11 +0100

    feat(settings): configure Celery settings in development and production environments

--- backend/project/settings/common.py
+++ backend/project/settings/common.py
@@ -166,13 +166,3 @@ SIMPLE_JWT = {
 #         "args": (False,)  # Set dry_run=False in production
 #     },
 # }
-
-
-
-
-# project/settings.py
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
