58e0f6ca8484dd2b5cd7d7fdad91c0ce5baf7400
Goldeno10 <ibrahimmuhammad271@gmail.com>
2026-03-06 08:05:27 +0100

    refactor(settings): streamline MongoDB connection and update environment configurations

--- backend/core/utils/db.py
+++ backend/core/utils/db.py
@@ -1,16 +1,13 @@
 import os
 from django.conf import settings
 from pymongo import MongoClient
-from dotenv import load_dotenv
 
-load_dotenv()
 
-MONGODB_URI = "mongodb://localhost:27017/"
 AUTH_DB_NAME = "datacube_V2_auth"
 
 class MongoConnection:
     def __init__(self):
-        self.client = MongoClient(MONGODB_URI)
+        self.client = MongoClient(settings.MONGODB_URI)
         self.db = self.client[AUTH_DB_NAME]
 
     def get_collection(self, collection_name):
--- backend/useful_cmd.sh
+++ backend/useful_cmd.sh
@@ -1,9 +1,10 @@
 DJANGO_SETTINGS_MODULE=project.settings.development uvicorn project.asgi:application --reload --host 127.0.0.1 --port 8000
+DJANGO_SETTINGS_MODULE=project.settings.production uvicorn project.asgi:application --reload --host 127.0.0.1 --port 8000
 
 docker run -d -p 6379:6379 redis:7-alpine
 
 docker run -d --name datacube-redis -p 6379:6379 redis:7-alpine
-celery -A project worker --loglevel=info -Q analytics,maintenance,celery --pool=gevent
+DJANGO_SETTINGS_MODULE=project.settings.development celery -A project worker --loglevel=info -Q analytics,maintenance,celery --pool=gevent
 
 celery -A project worker --loglevel=info -Q analytics,maintenance,celery
 
