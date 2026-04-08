3b38584c2d2632f33ecc3fc63a60d2232f0295bf
Goldeno10 <ibrahimmuhammad271@gmail.com>
2026-04-08 14:02:57 +0100

    refactor(docker): comment out image definitions in docker-compose for clarity

--- run_services.sh
+++ run_services.sh
@@ -8,3 +8,11 @@ docker compose -f docker-compose.prod_enhanced.yml build backend
 
 # 2. Then Build UI
 docker compose -f docker-compose.prod_enhanced.yml build react-app
+
+
+docker exec nginx nginx -s reload
+
+docker exec -it <backend_container_id> env | grep MONGO
+
+
+
