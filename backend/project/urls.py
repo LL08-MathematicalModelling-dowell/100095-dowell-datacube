57eade610d2aff06a8e9fe91d634f4a7462cbce4
Goldeno10 <ibrahimmuhammad271@gmail.com>
2026-04-09 17:51:47 +0100

    feat(api): add new URL configuration for API endpoints and remove deprecated urls.py refactor(settings): clean up production settings for CORS and security feat(nginx): enhance nginx configuration for analytics service and error handling chore(.gitignore): include IGNORE_THIS_DOC.md in gitignore

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('api/', include('api.urls')),
+    # path('api/', include('api.urls')),
     path('api/v2/', include('api.urls_v2')),
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
